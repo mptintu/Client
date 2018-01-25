b97cf187154161fdd130b7b442f7457106c7077c
Tintu <p.tintumol@yahoo.com>
2018-01-25 14:58:50 +0530

    index

--- index.js
+++ index.js
@@ -11,43 +11,43 @@ var serverPort = 8080;
 
 // swaggerRouter configuration
 var options = {
-  swaggerUi: path.join(__dirname, '/swagger.json'),
-  controllers: path.join(__dirname, './controllers'),
-  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
+    swaggerUi: path.join(__dirname, '/swagger.json'),
+    controllers: path.join(__dirname, './controllers'),
+    useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var spec = fs.readFileSync(path.join(__dirname, 'api/swagger.yaml'), 'utf8');
 var swaggerDoc = jsyaml.safeLoad(spec);
 
 // Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+swaggerTools.initializeMiddleware(swaggerDoc, function(middleware) {
 
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    app.use(middleware.swaggerMetadata());
 
-  // Validate Swagger requests
-  //app.use(middleware.swaggerValidator());
+    // Validate Swagger requests
+    //app.use(middleware.swaggerValidator());
 
-  // Route validated requests to appropriate controller
-  app.use(middleware.swaggerRouter(options));
+    // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
 
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());
+    // Serve the Swagger documents and Swagger UI
+    app.use(middleware.swaggerUi());
 
-   // Create an error handler to allow me to process errors before being sent to the client
-  //app.use(function (err, req, res, next) {
+    // Create an error handler to allow me to process errors before being sent to the client
+    //app.use(function (err, req, res, next) {
     // All errors that swagger-tools creates/handles are sent downstream via `next`.  This
     // means that all errors swagger-tools creates/handles will be sent here and can be
     // handled as your application deems fit.
     //console.log(err.stack)
     //res.status(200).send('Something broke!')
-  //});
+    //});
 
-  // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-  });
+    // Start the server
+    http.createServer(app).listen(serverPort, function() {
+        console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+        console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    });
 
 });
