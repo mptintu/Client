7fa968e89b6dee973d295b0347380b893ad7a699
Tintu <p.tintumol@yahoo.com>
2018-01-30 12:15:00 +0530

    client listing

--- controllers/Client.js
+++ controllers/Client.js
@@ -1,15 +1,20 @@
 'use strict';
-
+//var express = require('express');
+//var app = express();
 var url = require('url');
 var Client = require('./ClientService.js');
 var util = require('util');
 
 /*
+	console.log(JSON.parse(JSON.stringify(req.swagger.params.parameters.value)));
+	console.log("----------------------------------------------------");
 	console.log(req.swagger.params.parameters.value.end);
-	console.log(req.swagger.params.parameters.value.start_qrycnt);
+	console.log("////////////////"+req.swagger.params.parameters.value.start_qrycnt);
 	console.log("--------------------");
 */
-module.exports.clientFullListing = function clientFullListing (req, res, next) {
+
+
+module.exports.clientFullListing = function clientFullListing (req, res, next) {	
   Client.clientFullListing(req.swagger.params, res, next);
 };
 
